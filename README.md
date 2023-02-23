a801551a705986bd78e4a83a038c5939687a5f1c
Gruf1 <grufcs@yandex.kz>
2023-02-24 00:59:24 +0600

    step 3

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Gruf1/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/Gruf1/frontend-project-46/actions)
+
+## Example of Gendiff
+[![asciicast](https://asciinema.org/a/7NZrqUC9vPMi4sDxO5vYVTXVj.svg)](https://asciinema.org/a/7NZrqUC9vPMi4sDxO5vYVTXVj)
---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import compare from "../src/compare.js";
+
+const program = new Command();
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('1.0.0')
+    .arguments('<filepath1> <filepath2>')
+    .option('-f, --format <type>', 'output format')
+    .action((filepath1, filepath2) => {
+        console.log(compare(filepath1, filepath2, program.opts().format))
+    });
+program.parse();
